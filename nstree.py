67d9c7412167bbac8ff3cdf8b5809f0aed67bfae
ioef <iosifidise@gmail.com>
2017-09-05 22:27:23 +0300

    Major Improvements.
    Currently Showing PID and NAME

--- nstree.py
+++ nstree.py
@@ -47,6 +47,51 @@ def getSocket(data):
     return getIP(host), convHex2Dec(port)
 
 
+def findPIDs():
+    #create a list with the file/folder contents of /proc
+    processFiles = os.listdir("/proc/")
+    
+    #remove the ProcessID of the current Process
+    processFiles.remove(str(os.getpid()))
+
+    #create a list with process id files
+    pids = []
+
+    for filename in processFiles:
+        try:
+             #convert the filename to integer and append it to the pids list if it's a pid
+             integer = int(filename)
+             pids.append(str(integer))
+
+        except ValueError:
+             #if the filename doesn't convert to integer it's not a pid. Skip it.
+             pass
+
+    return pids
+
+def findInode(inode, pids):
+
+     #use the created pids list
+     for pid in pids:
+         #list dir for each process id to get the File Descriptors 
+         fDescriptors = os.listdir('/proc/%s/fd/' %pid)
+        
+         #for each pid and filedescriptor search for the socket with the relave Inode
+         for fd in fDescriptors:
+             pidFile = '/proc/%s/fd/%s' % (pid, fd)
+             #dereference symbolic link and check if it is equal to the inode
+             if os.path.exists(pidFile):
+                 if os.readlink(pidFile) == ('socket:[%d]' %inode):
+                     return pid
+
+    
+def getprocessName(pid):
+    if pid:
+        path = '/proc/' + pid + '/comm'
+        with open(path, 'r') as cmdFile:
+            return cmdFile.read().strip('\n')
+    return None
+
 tcpData = fetchData()
 
 
@@ -59,17 +104,23 @@ for line in tcpData:
     line = line.split(' ')
     #remove empty strings
     line = [x for x in line if x!='']
-    
+   
+
     #Local IP and Port
     lhost = getSocket(line[1])[0]
     lport = getSocket(line[1])[1]
 
+    #Acquire CONNECTION STATE from line
     state = State[line[3]]
-
-
-
+    
+    #Acquire Inode from line
+    inode = int(line[9])
+    
+    pids = findPIDs()
+    pid  = findInode(inode ,pids)
+   
     #prepare the Dictionary
-    datList = [[lport,state]]
+    datList = [[lport, state, pid]]
     if lhost in netstat:
         netstat[lhost].append(datList)
     else:
@@ -79,4 +130,7 @@ for line in tcpData:
 for key, values in netstat.items():
     print key.ljust(16,'-') + '+'
     for item in values:
-        print ' '.ljust(16)+ '|----> '+ str(item[0][0]) +' ' + str(item[0][1])
+      
+        print ' '.ljust(16)+ '|----> '+ 'port:'+ str(item[0][0]).ljust(10) + ' '\
+                + str(item[0][1]) + ' PID:' + str(item[0][2]) + ' NAME:' \
+                + getprocessName(item[0][2]) 
